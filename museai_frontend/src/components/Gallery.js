f08cfb466389052dd7ba0564579ed5c24cb31496
Binh Ho <binh.t.ho@gmail.com>
2022-10-22 00:02:47 -0500

    first prototype

--- museai_frontend/src/components/Gallery.js
+++ museai_frontend/src/components/Gallery.js
@@ -1,24 +1,91 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../gallery.css'
+import CloseIcon from '@material-ui/icons/Close'
 
-import Img from '../assets/alarm.png'
+import Img1 from '../assets/alarm.png'
+import Img2 from '../assets/bday.png'
+import Img3 from '../assets/castle.png'
+import Img4 from '../assets/city.png'
+import Img5 from '../assets/organ.png'
+import Img6 from '../assets/wolf.png'
+
 const Gallery = () => {
     let data = [
         {
-            img: Img,
+            img: Img1,
+            audio: '',
+            user: '',
+            prompt: ''
+        },
+        {
+            img: Img2,
+            audio: '',
+            user: '',
+            prompt: ''
+        },
+        {
+            img: Img3,
+            audio: '',
+            user: '',
+            prompt: ''
+        },
+        {
+            img: Img4,
+            audio: '',
+            user: '',
+            prompt: ''
+        },
+        {
+            img: Img5,
+            audio: '',
+            user: '',
+            prompt: ''
+        },
+        {
+            img: Img6,
             audio: '',
             user: '',
             prompt: ''
         }
     ]
+
+    const [modal, setModal] = useState(false)
+    const [imgSrc, setImgSrc] = useState('')
+    const getImg = (img) => {
+        setImgSrc(img)
+        setModal(true)
+        console.log(imgSrc)
+    }
+
     return (
-        <div className='gallery'>
-            {data.map((item, index)=>{
-                return (
-                    <div className='thumbnail' key={index}>
-                        <img src={item.img}/>
+        <div>
+            <div className={modal ? 'modal open' : 'modal'}>
+                <CloseIcon onClick={()=>setModal(false)}/>
+                <div className='container'>
+                    {/* <div className='row'>
+                        
+                    </div> */}
+                    <div className='row'>
+                        <div className='col-sm-8 justify-content-start'>
+                            {/* <h1 style={{color:}}>Caption</h1> */}
+                            <img src={imgSrc}/>
+                        </div>
+
                     </div>
-                )
-            })}
+                    
+                </div>
+                
+                
+            </div>
+            <div className='gallery'>
+                {data.map((item, index)=>{
+                    return (
+                        <div className='thumbnail' key={index} onClick={()=> getImg(item.img)}>
+                            <img src={item.img} style={{width: '100%'}}/>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
 
     )
